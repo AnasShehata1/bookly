d293b454e2bd5e54c6b7d7687ffb3c94cdebc6fe
anasmohamed <manas9109@gmail.com>
2024-09-12 00:06:26 +0300

    refactor navigation in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'Features/splash/presentation/views/splash_view.dart';
 import 'constants.dart';
 
 void main() {
   runApp(const Bookly());
 }
 
+
 class Bookly extends StatelessWidget {
   const Bookly({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: kPrimaryColor,
           textTheme:
               GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
-      home: const SplashView(),
     );
   }
 }
